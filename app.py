e3387fbb64f0f46ad0204f2f316235ad60aa38b1
Krampus030 <xcy2102231921@outlook.com>
2025-02-07 17:21:04 +0000

    change the basic structure, now the app has two different stages: pre login and post login, thus the user can only use the main command after login

--- app.py
+++ app.py
@@ -1,85 +1,80 @@
-from login import User
-import sys
+class PreAccountCLI:
+    """Handles user registration and login commands."""
 
-
-class Command:
-    def __init__(self, CLI):
-        self.cli = CLI
-        self.user = User()
-
-    def execute(self, args):
-        raise NotImplementedError("Child class must have execute method")
-
-
-class Help(Command):
-
-    def execute(self, args):
-        if not self.cli.user.logged_in_user:
-            print("Error, please login first!")
-            return False
-        print("Available command:")
-        print("  help          Show all commands")
-        print("  register      Open a new account")
-        print("  login         Login")
-        print("  logout        Log out")
-
-
-
-class Register(Command):
-    def execute(self, args):
-        username = input("Enter user name:\n")
-        password = input("Enter password:\n")
-        self.user.register(username, password)
-
-
-class Login(Command):
-    def execute(self, args):
-        username = input("Enter user name:\n")
-        password = input("Enter password:\n")
-        self.user.login(username, password)
-
-
-class Logout(Command):
-    def execute(self, args):
-        if self.cli.user.logged_in_user:
-            self.cli.user.logout()
-        else:
-            print("No user is logged in.")
-
-
-
-class CLIApp:
-    def __init__(self):
-        self.user = User()
-
+    def __init__(self, user_instance):
+        self.user = user_instance
         self.commands = {
-            "help": Help(self),
-            "register": Register(self),
-            "login": Login(self),
-            "logout": Logout(self)
-            # "":
-            # "":
-            # "":
+            "register": self.register,
+            "login": self.login,
+            "help": self.help,
+            "logout": self.logout
         }
 
-    def run(self):
-        if len(sys.argv) < 2:
-            print("Error: Please enter command. For example 'app.py help")
-            return
+    def register(self):
+        username = input("Enter username: ")
+        password = input("Enter password(should between 8 to 16 char): ")
+        initial_deposit = int(input("Enter initial deposit: "))
+        self.user.register(username, password, initial_deposit)
 
-        command_name = sys.argv[1]
-        args = sys.argv[2:]
+    def login(self):
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        self.user.login(username, password)
 
-        command = self.commands.get(command_name)
+    def logout(self):
+        self.user.logout()
 
-        if command:
-            command.execute(args)
-        else:
-            print(f"Unknown command {command_name}")
-            print("Enter app.py help to check available commands")
+    def help(self):
+        print("\nAvailable commands:")
+        print("  register  - Register a new user")
+        print("  login     - Log in to an existing account")
+        print("  logout    - Can not log out since not logged in yet")
+        print("  help      - Show this help message\n")
+
+
+class PostAccountCLI:
+    """Handles operations available after login."""
+
+    def __init__(self, user_instance):
+        self.user = user_instance
+        self.commands = {
+            "check_balance": self.check_balance,
+            "deposit": self.deposit,
+            "withdraw": self.withdraw,
+            "transfer": self.transfer,
+            "logout": self.logout,
+            "help": self.help
+        }
+
+    def check_balance(self):
+        print(f"Balance: {self.user.check_balance()}")
+
+    def deposit(self):
+        amount = int(input("Enter deposit amount: "))
+        self.user.deposit(amount)
+
+    def withdraw(self):
+        amount = int(input("Enter withdrawal amount: "))
+        self.user.withdraw(amount)
+
+    def transfer(self):
+        target_user = input("Enter target username: ")
+        amount = int(input("Enter transfer amount: "))
+        self.user.transfer(target_user, amount)
+
+    def logout(self):
+        self.user.logout()
+
+    def help(self):
+        print("\nAvailable commands:")
+        print("  check_balance  - View account balance")
+        print("  deposit        - Add money to account")
+        print("  withdraw       - Withdraw money")
+        print("  transfer       - Transfer money to another user")
+        print("  logout         - Log out of the account")
+        print("  help           - Show this help message\n")
 
 
 
-if __name__ == "__main__":
-    app = CLIApp()
-    app.run()
+
+
